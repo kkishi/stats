23c972d27a8c238dfeb1849a0e72bfe2c4c6fa2a
Keisuke Kishimoto <keisuke.kishimoto@gmail.com>
2017-12-31 13:41:35 +0900

    Add standard deviation and correlation coefficient

--- stats.go
+++ stats.go
@@ -1,5 +1,7 @@
 package stats
 
+import "math"
+
 func Sum(xs []float64) float64 {
 	var f float64
 	for _, x := range xs {
@@ -13,4 +15,45 @@ func ArithmeticMean(xs []float64) float64 {
 		return 0
 	}
 	return Sum(xs) / float64(len(xs))
+}
+
+func square(x float64) float64 {
+	return x * x
+}
+
+func SquaredDeviationSum(xs []float64) float64 {
+	m := ArithmeticMean(xs)
+	var s float64
+	for _, x := range xs {
+		s += square(x - m)
+	}
+	return s
+}
+
+func Variance(xs []float64) float64 {
+	if len(xs) == 0 {
+		return 0
+	}
+	return SquaredDeviationSum(xs) / float64(len(xs))
+}
+
+func StandardDeviation(xs []float64) float64 {
+	return math.Sqrt(Variance(xs))
+}
+
+func CoefficientOfVariation(xs []float64) float64 {
+	m := ArithmeticMean(xs)
+	if m == 0 {
+		return 0
+	}
+	return StandardDeviation(xs) / m
+}
+
+func CorrelationCoefficient(xs []float64, ys []float64) float64 {
+	xm, ym := ArithmeticMean(xs), ArithmeticMean(ys)
+	var s float64
+	for i, x := range xs {
+		s += (x - xm) * (ys[i] - ym)
+	}
+	return s / math.Sqrt(SquaredDeviationSum(xs)*SquaredDeviationSum(ys))
 }
